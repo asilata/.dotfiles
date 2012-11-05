5ae1240b6eecfa170286cef6a842b109b0a6d0a5
Asilata Bapat <asilata@gmail.com>
2012-11-04 20:36:23 -0600

    Added uniquify for better titling of buffers with the same name.

--- init.el
+++ init.el
@@ -64,6 +64,12 @@
                                             (abbreviate-file-name (buffer-file-name)))
                                         "%b")))
 
+(require 'uniquify) ;; For better naming of buffers with the same name.
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
 ;;; Colour themes
 (load-theme 'zenburn t)
 
