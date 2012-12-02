31ba11b5b4e5d4d9e8ca78f2969ab5b4d363e01f
Asilata Bapat <asilata@gmail.com>
2012-12-02 14:35:36 -0600

    Added switch-to-workspace.zsh and made some minor changes in xmonad.hs.

---
+++ switch-to-workspace.zsh
@@ -0,0 +1,14 @@
+#! /bin/zsh
+
+if [ $# -eq 1 ]
+then
+    /usr/bin/wmctrl -d | while read line;
+    do
+        WSNUM=`echo $line | awk '{ print $1 }'`
+        WSTAG=`echo $line | awk '{ print $10 }'`
+        if [ "$1" = "$WSTAG" ]
+        then
+            wmctrl -s $WSNUM
+        fi
+    done
+fi
