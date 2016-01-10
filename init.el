904618bbb436908983ba308ef21a63e56d401281
Asilata Bapat <asilata@gmail.com>
2016-01-10 15:57:39 -0600

    Added function to create new post.

--- init.el
+++ init.el
@@ -148,6 +148,7 @@
 (global-set-key [C-end]       'end-of-buffer)
 (global-set-key (kbd "C-x o") 'switch-window) ;; Visual window switching
 (global-set-key (kbd "C-;")   'toggle-comment-line-or-region)
+(global-set-key (kbd "C-x C-j") 'jekyll-new-post)
 
 ;;; Backup and cleanup
 ;; Back up files
@@ -201,8 +202,20 @@
   (setq web-mode-enable-auto-pairing t
         web-mode-enable-auto-pairing t))
 
-;; Hyde mode for writing jekyll stuff.
-;;(require 'hyde)
+;; Jekyll stuff (new post function, modified from hyde-mode's version)
+(defun jekyll-new-post (title directory)
+  "Creates a new post"
+  (interactive "MEnter post title: \nDEnter directory to save in: ")
+  (let ((post-file-name (expand-file-name (format "%s/%s.markdown"
+                                                  directory
+                                                  (concat (format-time-string "%Y-%m-%d-") (downcase (replace-regexp-in-string " " "-" title)))))))
+    (find-file post-file-name)
+    (insert "---\n")
+    (insert "layout: post\n")
+    (insert (format "title: \"%s\"\n" title))
+    (insert (format "date: \"%s\"\n" (format-time-string "%Y-%m-%d %H:%M:%S %z")))
+    (insert "---\n\n")
+    (markdown-mode)))
 
 ;;; Mode-specific hooks
 (use-package reftex)
@@ -266,17 +279,3 @@
 
 ;; Recompile all previously byte-compiled files in the directory.
 (byte-recompile-directory user-emacs-directory)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auctex-latexmk use-package auctex zenburn-theme web-mode volatile-highlights textile-mode switch-window smartparens scss-mode rainbow-mode python php+-mode paredit org muttrc-mode markdown-mode magit haskell-mode haml-mode autopair auto-complete-auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
