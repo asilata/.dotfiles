63e2c78d1235c371dd6c534d5a44448aae877d70
Asilata Bapat <asilata@gmail.com>
2018-03-08 10:54:14 +1100

    Lots of goodies (mainly sublayout), and removed GridSelect.

--- wm/xmonad.hs
+++ wm/xmonad.hs
@@ -1,3 +1,4 @@
+{-#LANGUAGE FlexibleContexts, TypeSynonymInstances, DeriveDataTypeable, MultiParamTypeClasses#-}
 --
 -- My xmonad configuration file.
 --
@@ -5,7 +6,7 @@
 import XMonad hiding ((|||))
 
 import XMonad.Actions.DynamicWorkspaces -- For creating/deleting workspaces dynamically.
-import XMonad.Actions.GridSelect
+import XMonad.Actions.MouseGestures
 
 import XMonad.Hooks.ManageDocks -- For managing specific windows.
 import XMonad.Hooks.ManageHelpers
@@ -15,16 +16,21 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
 
+import XMonad.Layout.BoringWindows
 import XMonad.Layout.Grid
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.Minimize
 import XMonad.Layout.Monitor
 import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
+import XMonad.Layout.SubLayouts
 import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.WindowNavigation
 
 import XMonad.Prompt
 import XMonad.Prompt.Shell
@@ -73,13 +79,6 @@ myXPConfig = def {
   borderColor = myActiveBorderColor,
   height = 40}
 
---myGSConfig :: GSConfig
-myGSConfig = def {
-  gs_cellheight = 75,
-  gs_cellwidth = 250,
-  gs_cellpadding = 20,
-  gs_font = myFont }
-
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here. Everything is emacs-style.
 --
@@ -88,15 +87,11 @@ myKeys = \conf -> mkKeymap conf
       ("M-r", shellPrompt myXPConfig), -- launch shell prompt
       ("M-S-c", kill), -- close focused window 
       ("M-<Space>", sendMessage NextLayout), -- Rotate through the available layout algorithms
-      ("M-f", sendMessage $ JumpToLayout "Tabbed Simplest"), -- Full layout
-      --("M-g", sendMessage $ JumpToLayout "Grid"), -- Grid layout
-      ("M-g", goToSelected myGSConfig),
+      ("M-f", sendMessage $ Toggle NBFULL), -- Toggle Full layout
+      ("M-g", sendMessage $ Toggle GRIDIFY), -- Toggle Grid layout
       ("M-k", namedScratchpadAction myScratchPads "konsole"),
       ("M-S-<Space>", setLayout $ XMonad.layoutHook conf), -- Reset to default layouts on the current workspace
       ("M-n", refresh), -- Resize viewed windows to the correct size
-      ("M-<Tab>", windows W.focusDown), -- Move focus to the next window
-      ("M-S-<Tab>", windows W.focusUp), -- Move focus to the previous window
-      ("M-m", windows W.focusMaster), -- Move focus to the master window
       ("M-<Return>", windows W.swapMaster), -- Swap the focused window and the master window
       ("M-S-j", windows W.swapDown), -- Swap the focused window with the next window
       ("M-S-k", windows W.swapUp), -- Swap the focused window with the previous window
@@ -109,11 +104,29 @@ myKeys = \conf -> mkKeymap conf
       ("M-S-q", io (exitWith ExitSuccess)), -- Quit xmonad
       ("M-q", restart "xmonad" True), -- Restart xmonad
       ("M-S-<Backspace>", removeWorkspace), -- Remove workspace
-      ("M-S-b", bringSelected myGSConfig), -- Bring selected window to this workspace
-      ("M-w", selectWorkspace myXPConfig), -- Select workspace to navigate to
+      ("M-w", selectWorkspace myXPConfig), -- Select workspace to navigate to 
       ("M-S-w", withWorkspace myXPConfig (windows . W.shift)), -- Move current window to selected workspace
       ("M-S-r", renameWorkspace def) -- Rename workspace
      ]
+  ++
+  [
+    ("M-C-h", sendMessage $ pullGroup L)
+  , ("M-C-l", sendMessage $ pullGroup R)
+  , ("M-C-k", sendMessage $ pullGroup U)
+  , ("M-C-j", sendMessage $ pullGroup D)
+    
+  , ("M-C-m", withFocused (sendMessage . MergeAll))
+  , ("M-C-u", withFocused (sendMessage . UnMerge))
+  
+  , ("M-C-.", onGroup W.focusUp')
+  , ("M-C-,", onGroup W.focusDown')
+  ]
+  ++
+  [
+    ("M-<Tab>", focusDown)
+  , ("M-S-<Tab>", focusUp)
+  , ("M-m", focusMaster)
+  ]
   ++
   -- mod-[1..9] switches to the appropriate workspace.
   map (\x -> ("M-" ++ show (x+1), withNthWorkspace W.greedyView x)) [0..8]
@@ -121,27 +134,62 @@ myKeys = \conf -> mkKeymap conf
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
-  [((modMask, button1), (\w -> focus w >> mouseMoveWindow w)), -- mod-button1, Set the window to floating mode and move by dragging
-   ((modMask, button2), (\w -> focus w >> windows W.swapMaster)), -- mod-button2, Raise the window to the top of the stack
-   ((modMask, button3), (\w -> focus w >> mouseResizeWindow w)) -- mod-button3, Set the window to floating mode and resize by dragging
+  [((modMask, button1), (\w -> focus w >> mouseMoveWindow w)) -- mod-button1, Set the window to floating mode and move by dragging
+  , ((modMask, button2), (\w -> focus w >> windows W.swapMaster)) -- mod-button2, Raise the window to the top of the stack
+  , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w)) -- mod-button3, Set the window to floating mode and resize by dragging
    -- you may also bind events to the mouse scroll wheel (button4 and button5)
   ]
+  ++
+  [
+    ((mod1Mask, button1), mouseGesture gestures)
+  ]
+
+gestures = M.fromList
+  [ ([], sendMessage . UnMerge)
+  , ([U], \_ -> sendMessage $ pullGroup U)
+  , ([D], \_ -> sendMessage $ pullGroup D)
+  , ([L], \_ -> sendMessage $ pullGroup L)
+  , ([R], \_ -> sendMessage $ pullGroup R)
+  ]
+-- myMouseBindings = \conf -> mkKeymap conf $
+--   [
+--     ("M-button1", )
+--   ]
 
 ------------------------------------------------------------------------
--- Layouts:
+-- layouts:
 
 myDecoTheme = def { 
   fontName = "-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*"
   }
 
 myHandleEventHook = minimizeEventHook <+> fullscreenEventHook
-myLayoutHook = myLayoutModifiers (grid ||| tiled ||| simpleTabbed)
-  where
-    -- default layout modifiers to be applied everywhere
-    myLayoutModifiers = (layoutHints . smartBorders . minimize . avoidStruts) 
-    -- Default tiling algorithm partitions the screen into two panes.
-    tiled   = spacing 2 $ Tall 1 (3/100) (1/2)
-    grid = spacing 2 $ Grid
+-- myLayoutHook = myLayoutModifiers (grid ||| tiled ||| simpleTabbed)
+--   where
+--     -- default layout modifiers to be applied everywhere
+--     myLayoutModifiers = (layoutHints . smartBorders . minimize . avoidStruts) 
+--     -- Default tiling algorithm partitions the screen into two panes.
+--     tiled   = spacing 2 $ Tall 1 (3/100) (1/2)
+--     grid = spacing 2 $ Grid
+data GRIDIFY = GRIDIFY deriving (Read, Show, Eq, Typeable)
+instance Transformer GRIDIFY Window where
+  transform GRIDIFY x k = k (GridRatio (4/3)) (const x)
+
+myLayoutHook = myLayoutModifiers (tall ||| threeColMid)
+  where myLayoutModifiers =
+          (layoutHints
+           . smartBorders
+           . minimize
+           . avoidStruts
+           . windowNavigation
+           . boringAuto
+           . (mkToggle (single NBFULL))
+           . (mkToggle (single GRIDIFY))
+           . subTabbed
+          )
+        tall = Tall 1 (3/100) (1/2)
+        threeColMid = ThreeColMid 1 (1/100) (1/2)
+
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -157,7 +205,6 @@ myManageHook = composeAll . concat $
                ]
   where
     myFloats = ["SMPlayer", "MPlayer", "Krunner", "Plugin-container", "Redshiftgui", "plasma", "Plasma", "plasma-desktop", "Plasma-desktop", "plasmashell", "Yakuake"]
-    --myFloats = []
     myFullFloats = ["Xournal"]
 
 -- Whether focus follows the mouse pointer.
