3953a8f111089084550e18e6e045172a60d41264
Asilata Bapat <asilata@gmail.com>
2018-11-08 23:55:44 +1100

    Checkboxes auto-done function.

--- org-mode-config.el
+++ org-mode-config.el
@@ -118,3 +118,25 @@
   (setq org-journal-file-format "%Y-%m-%d.org")
   )
 
+;; Custom functionality, etc
+
+(eval-after-load 'org-list
+  '(add-hook 'org-checkbox-statistics-hook (function auto-done-checkboxes)))
+
+(defun auto-done-checkboxes ()
+  (save-excursion
+    (org-back-to-heading t)
+    (let ((beg (point)) end)
+      (end-of-line)
+      (setq end (point))
+      (goto-char beg)
+      (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
+            (if (match-end 1)
+                (if (equal (match-string 1) "100%")
+                    ;; all done - do the state change
+                    (org-todo 'done)
+                  (org-todo 'todo))
+              (if (and (> (match-end 2) (match-beginning 2))
+                       (equal (match-string 2) (match-string 3)))
+                  (org-todo 'done)
+                (org-todo 'todo)))))))
