06eb2cd0a1da1105e53f6dfaaeb76b613920b416
Asilata Bapat <asilata@gmail.com>
2019-07-04 22:12:33 +1000

    Updated multiple cursors keybindings.

--- init.el
+++ init.el
@@ -230,7 +230,10 @@
 
 (use-package multiple-cursors
   :ensure t
-  :bind (("C-c m c" . mc/edit-lines)))
+  :bind (("C-c m c" . mc/edit-lines)
+         ("C-c m n" . mc/mark-next-like-this)
+         ("C-c m p" . mc/mark-previous-like-this)
+         ("C-c m a" . mc/mark-all-like-this)))
 
 (use-package sage-shell-mode
   :ensure t
