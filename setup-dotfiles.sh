0aff0bea83393b5f524e0f497968f268859f7b8c
Asilata Bapat (peanut) <asilata@gmail.com>
2019-10-04 20:57:31 +1000

    Upgraded setup shell script.

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -2,31 +2,33 @@
 
 DOTFILES=~/.dotfiles
 
-# Emacs
-if [ ! -d "~/.emacs.d/"]; then
+# * Emacs
+if [ ! -d "$HOME/.emacs.d/" ]; then
     mkdir ~/.emacs.d
     mkdir ~/.emacs.d/opt
 fi
 ln -s $DOTFILES/init.el ~/.emacs.d/init.el
 ln -s $DOTFILES/org-mode-config.el ~/.emacs.d/opt/org-mode-config.el
 
-# Email stuff
+# * Email stuff
 ln -s $DOTFILES/email/mu4e-config.el ~/.emacs.d/opt/mu4e-config.el
 ln -s $DOTFILES/email/mbsyncrc ~/.mbsyncrc
 
-# ZSH
+# * ZSH
 ln -s $DOTFILES/zshrc ~/.zshrc
 
-# WM
-if [ ! -d "~/.xmonad"]; then
+# * WM
+if [ ! -d "$HOME/.xmonad" ]; then
     mkdir ~/.xmonad
 fi
 ln -s $DOTFILES/wm/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s $DOTFILES/wm/exwm-config-file.el ~/.emacs.d/opt/exwm-config-file.el
 
-# Gitignore
+# * Gitignore
+git config --global core.excludesfile "$HOME/.gitignore_global"
 ln -s $DOTFILES/gitignore_global ~/.gitignore_global
 
-# Sets up everything except tablet scripts. To set up tablet scripts, uncomment the following.
+# * To set up tablet scripts, uncomment the following.
 # sudo ln -s $DOTFILES/tablet/cw-rotate /usr/local/bin/cw-rotate
 # sudo ln -s $DOTFILES/tablet/toggle-tablet /usr/local/bin/toggle-tablet
 
